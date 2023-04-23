bfc44f938a7d762ec043b6e2f8603bcc4d437e7c
Wonxen <85366208+Wonxen@users.noreply.github.com>
2023-04-23 09:12:47 +0300

    Delete src directory

--- src/database/xp.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Stats = mongoose.Schema({
-    sunucuid: String,
-    kulid: String,
-    xpdata: { type: Number, default: 0 }
-  });
-
-module.exports = mongoose.model("UyeXP", Stats);
